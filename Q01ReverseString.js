8d6c4cfee7518dd6837ae292567a9891d3edae66
aniket-6234 <aniketashok1999@gmail.com>
2022-07-13 14:14:03 +0530

    Reverse the String

--- Q01ReverseString.js
+++ Q01ReverseString.js
@@ -2,7 +2,26 @@
 // -> "Hello" -> "olleH"
 
 const reverseString = str => {
-  return str.split('').reverse().join('');
+
+  // Method 1 ->
+  // return str.split('').reverse().join('');
+
+
+  // Method 2 ->
+  // let res = "";
+  // for(let i = 0; i < str.length; i++) {
+  //    const char = str[i];
+  //    res = char + res;
+  // }
+  // return res;
+
+  
+  // Method 3 ->
+  return str.split("").reduce((output, char) => {
+    output = char + output;
+    return output;
+  }, "");
+
 }
 
 const str = "JavaScript";
