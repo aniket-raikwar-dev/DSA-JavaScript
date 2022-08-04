829b2d55eb6715db3b91bd63b30f31dcbabdbf70
aniket-6234 <aniketashok1999@gmail.com>
2022-08-04 17:22:09 +0530

    Stack

---
+++ Data Structure/DS1_Stack.js
@@ -0,0 +1,48 @@
+// Stack Data Structure
+
+class Stack {
+  constructor() {
+    this.items = []
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+
+console.log(stack.isEmpty());
+stack.push(20);
+stack.push(45);
+stack.push(11);
+stack.push(89);
+console.log(stack.size());
+stack.print();
+
+// pop
+stack.pop();
+stack.print();
+
+// peek
+console.log(stack.peek());
